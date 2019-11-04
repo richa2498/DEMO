a4abd8c6c7763d3e879fd618c71fce4532e3bdd3
MacStudent <macstudent@126-iMac36.local>
2019-11-04 15:06:32 -0500

    first commit

---
+++ newone/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  newone
+//
+//  Created by MacStudent on 2019-11-04.
+//  Copyright © 2019 MacStudent. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
